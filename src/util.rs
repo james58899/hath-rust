84d4831370fd7ee6f3aa1e72617e9c0be5cc1508
james58899 <james59988@gmail.com>
2022-05-24 14:29:06 +0000

    Allow connection keepalive

--- src/util.rs
+++ src/util.rs
@@ -2,7 +2,6 @@ use std::time::Duration;
 
 use futures::future::try_join_all;
 use openssl::sha::Sha1;
-use reqwest::header::{self, HeaderMap, HeaderValue};
 use tokio::fs::create_dir_all;
 
 use crate::CLIENT_VERSION;
@@ -14,15 +13,13 @@ pub fn string_to_hash(str: String) -> String {
 }
 
 pub fn create_http_client() -> reqwest::Client {
-    let mut headers = HeaderMap::new();
-    headers.insert(header::CONNECTION, HeaderValue::from_static("Close"));
-
     reqwest::ClientBuilder::new()
         .user_agent(format!("Hentai@Home {}", CLIENT_VERSION))
         .tcp_keepalive(Duration::from_secs(75)) // Linux default keepalive inverval
         .connect_timeout(Duration::from_secs(5))
         .timeout(Duration::from_secs(10))
-        .default_headers(headers)
+        .pool_idle_timeout(Duration::from_secs(3600))
+        .pool_max_idle_per_host(8)
         .http1_title_case_headers()
         .http1_only()
         .build()
