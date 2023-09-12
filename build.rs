d8b0deba5e95955a0f70d6b1661653500fc896a7
james58899 <james59988@gmail.com>
2023-09-12 01:18:25 +0000

    Update built

--- build.rs
+++ build.rs
@@ -1,14 +1,3 @@
-use std::{env, path::Path};
-
-use built::Options;
-
 fn main() {
-    let mut options = Options::default();
-    options.set_compiler(false).set_ci(false).set_features(false).set_cfg(false);
-    built::write_built_file_with_opts(
-        &options,
-        env::var("CARGO_MANIFEST_DIR").unwrap().as_ref(),
-        &Path::new(&env::var("OUT_DIR").unwrap()).join("built.rs"),
-    )
-    .expect("Failed to acquire build-time information");
+    built::write_built_file().expect("Failed to acquire build-time information");
 }
