72ab3970d7e89dd0caec1bbd9cb8692e5e221d72
james58899 <james59988@gmail.com>
2024-06-26 07:12:15 +0000

    Update tikv-jemallocator
    New version allow musl enable unprefixed malloc feature and
    also fix android build.

--- src/main.rs
+++ src/main.rs
@@ -43,14 +43,17 @@ mod rpc;
 mod server;
 mod util;
 
-#[cfg(not(any(target_env = "msvc", target_env = "musl", target_os = "android")))]
+#[cfg(not(any(target_env = "msvc")))]
 #[global_allocator]
 static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 
 // jemalloc config
-#[cfg(not(any(target_env = "msvc", target_env = "musl")))]
+#[cfg(not(any(target_env = "msvc")))]
 #[no_mangle]
 pub static mut malloc_conf: *const u8 = b"percpu_arena:phycpu,tcache:false,dirty_decay_ms:1000,muzzy_decay_ms:0\0".as_ptr();
+#[cfg(any(target_os = "android", target_os = "macos"))]
+#[no_mangle]
+pub static mut _rjem_malloc_conf: *const u8 = b"percpu_arena:phycpu,tcache:false,dirty_decay_ms:1000,muzzy_decay_ms:0\0".as_ptr();
 
 const VERSION: &str = concat!(env!("CARGO_PKG_VERSION"), "-", env!("VERGEN_GIT_SHA"));
 pub const CLIENT_VERSION: &str = "1.6.3";
