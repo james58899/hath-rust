cc6a5a51940843d38b8720e2b5a37aa3cfc3432a
james58899 <james59988@gmail.com>
2025-03-27 17:38:26 +0000

    Merge ssl mod into server

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -14,8 +14,14 @@ use futures::pin_mut;
 use hyper::server::conn::http1;
 use hyper_util::rt::{TokioIo, TokioTimer};
 use log::info;
-use rustls::ServerConfig;
-use ssl::ParsedCert;
+use p12::PFX;
+use rustls::{
+    ServerConfig,
+    compress::CompressionCache,
+    crypto::aws_lc_rs::Ticketer,
+    pki_types::{CertificateDer, PrivateKeyDer, PrivatePkcs8KeyDer},
+    server::NoServerSessionStorage,
+};
 use tokio::{
     net::{TcpListener, TcpSocket, TcpStream},
     sync::{Notify, watch},
@@ -25,9 +31,10 @@ use tokio::{
 use tokio_rustls::TlsAcceptor;
 use tower::{Layer, Service};
 
-use crate::{AppState, middleware, route, server::ssl::create_ssl_config};
-
-pub mod ssl;
+use crate::{
+    AppState, middleware, route,
+    util::{aes_support, ssl_provider},
+};
 
 pub struct Server {
     handle: Arc<ServerHandle>,
@@ -198,4 +205,39 @@ async fn accept(listener: &mut TcpListener) -> (TcpStream, SocketAddr) {
             Err(_) => tokio::time::sleep(Duration::from_millis(50)).await, // Too many open files, retry.
         }
     }
+}
+
+pub struct ParsedCert {
+    certs: Vec<CertificateDer<'static>>,
+    key: PrivateKeyDer<'static>,
+}
+
+impl ParsedCert {
+    pub fn from_p12(p12: &[u8], password: &str) -> Option<ParsedCert> {
+        let pfx = PFX::parse(p12).ok()?;
+        let key = PrivatePkcs8KeyDer::from(pfx.key_bags(password).ok()?.pop()?).into();
+        let certs: Vec<CertificateDer> = pfx.cert_x509_bags(password).ok()?.into_iter().map(CertificateDer::from).collect();
+
+        Some(ParsedCert { certs, key })
+    }
+}
+
+pub fn create_ssl_config(cert: ParsedCert) -> ServerConfig {
+    // TODO error handle
+    let mut config = ServerConfig::builder_with_provider(ssl_provider().into())
+        .with_safe_default_protocol_versions()
+        .unwrap()
+        .with_no_client_auth()
+        .with_single_cert(cert.certs, cert.key)
+        .unwrap();
+
+    // Prefer ChaCha20 on non-AES hardware.
+    config.ignore_client_order = !aes_support();
+    // Only support ticket resumption.
+    config.session_storage = Arc::new(NoServerSessionStorage {});
+    config.ticketer = Ticketer::new().unwrap();
+    config.send_tls13_tickets = 1;
+    config.cert_compression_cache = CompressionCache::new(2).into(); // 1 cert * 2 compression algorithms
+
+    config
 }
