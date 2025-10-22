9b14eab15287b6474502540cd10921ca9cfe7733
james58899 <james59988@gmail.com>
2025-10-22 18:06:36 +0800

    Patch h3-quinn to wait peer receive all data

---
+++ src/server/h3_quinn.rs
@@ -0,0 +1,455 @@
+// Copy from https://github.com/hyperium/h3/blob/e523eaf3f0dc8a8e0df4cd10f71c9d00809ea666/h3-quinn/src/lib.rs
+// Add call stopped() in poll_finish() to wait peer receive all data
+
+//! QUIC Transport implementation with Quinn
+//!
+//! This module implements QUIC traits with Quinn.
+#![deny(missing_docs)]
+
+use std::{
+    convert::TryInto,
+    future::Future,
+    pin::Pin,
+    sync::Arc,
+    task::{self, Poll},
+};
+
+use bytes::{Buf, Bytes};
+use futures::{
+    FutureExt, Stream, StreamExt,
+    future::BoxFuture,
+    ready,
+    stream::{self},
+};
+use h3::{
+    error::Code,
+    quic::{self, ConnectionErrorIncoming, StreamErrorIncoming, StreamId, WriteBuf},
+};
+use quinn::ReadError;
+pub use quinn::{self, AcceptBi, AcceptUni, OpenBi, OpenUni, VarInt};
+use tokio_util::sync::ReusableBoxFuture;
+
+/// BoxStream with Sync trait
+type BoxStreamSync<'a, T> = Pin<Box<dyn Stream<Item = T> + Sync + Send + 'a>>;
+
+/// A QUIC connection backed by Quinn
+///
+/// Implements a [`quic::Connection`] backed by a [`quinn::Connection`].
+pub struct Connection {
+    conn: quinn::Connection,
+    incoming_bi: BoxStreamSync<'static, <AcceptBi<'static> as Future>::Output>,
+    opening_bi: Option<BoxStreamSync<'static, <OpenBi<'static> as Future>::Output>>,
+    incoming_uni: BoxStreamSync<'static, <AcceptUni<'static> as Future>::Output>,
+    opening_uni: Option<BoxStreamSync<'static, <OpenUni<'static> as Future>::Output>>,
+}
+
+impl Connection {
+    /// Create a [`Connection`] from a [`quinn::Connection`]
+    pub fn new(conn: quinn::Connection) -> Self {
+        Self {
+            conn: conn.clone(),
+            incoming_bi: Box::pin(stream::unfold(conn.clone(), |conn| async { Some((conn.accept_bi().await, conn)) })),
+            opening_bi: None,
+            incoming_uni: Box::pin(stream::unfold(conn.clone(), |conn| async { Some((conn.accept_uni().await, conn)) })),
+            opening_uni: None,
+        }
+    }
+}
+
+impl<B> quic::Connection<B> for Connection
+where
+    B: Buf,
+{
+    type OpenStreams = OpenStreams;
+    type RecvStream = RecvStream;
+
+    fn poll_accept_bidi(&mut self, cx: &mut task::Context<'_>) -> Poll<Result<Self::BidiStream, ConnectionErrorIncoming>> {
+        let (send, recv) = ready!(self.incoming_bi.poll_next_unpin(cx))
+            .expect("self.incoming_bi BoxStream never returns None")
+            .map_err(convert_connection_error)?;
+        Poll::Ready(Ok(Self::BidiStream {
+            send: Self::SendStream::new(send),
+            recv: Self::RecvStream::new(recv),
+        }))
+    }
+
+    fn poll_accept_recv(&mut self, cx: &mut task::Context<'_>) -> Poll<Result<Self::RecvStream, ConnectionErrorIncoming>> {
+        let recv = ready!(self.incoming_uni.poll_next_unpin(cx))
+            .expect("self.incoming_uni BoxStream never returns None")
+            .map_err(convert_connection_error)?;
+        Poll::Ready(Ok(Self::RecvStream::new(recv)))
+    }
+
+    fn opener(&self) -> Self::OpenStreams {
+        OpenStreams {
+            conn: self.conn.clone(),
+            opening_bi: None,
+            opening_uni: None,
+        }
+    }
+}
+
+fn convert_connection_error(e: quinn::ConnectionError) -> h3::quic::ConnectionErrorIncoming {
+    match e {
+        quinn::ConnectionError::ApplicationClosed(application_close) => ConnectionErrorIncoming::ApplicationClose {
+            error_code: application_close.error_code.into(),
+        },
+        quinn::ConnectionError::TimedOut => ConnectionErrorIncoming::Timeout,
+
+        error @ quinn::ConnectionError::VersionMismatch
+        | error @ quinn::ConnectionError::Reset
+        | error @ quinn::ConnectionError::LocallyClosed
+        | error @ quinn::ConnectionError::CidsExhausted
+        | error @ quinn::ConnectionError::TransportError(_)
+        | error @ quinn::ConnectionError::ConnectionClosed(_) => ConnectionErrorIncoming::Undefined(Arc::new(error)),
+    }
+}
+
+impl<B> quic::OpenStreams<B> for Connection
+where
+    B: Buf,
+{
+    type BidiStream = BidiStream<B>;
+    type SendStream = SendStream<B>;
+
+    fn poll_open_bidi(&mut self, cx: &mut task::Context<'_>) -> Poll<Result<Self::BidiStream, StreamErrorIncoming>> {
+        let bi = self
+            .opening_bi
+            .get_or_insert_with(|| Box::pin(stream::unfold(self.conn.clone(), |conn| async { Some((conn.open_bi().await, conn)) })));
+        let (send, recv) = ready!(bi.poll_next_unpin(cx))
+            .expect("BoxStream does not return None")
+            .map_err(|e| StreamErrorIncoming::ConnectionErrorIncoming {
+                connection_error: convert_connection_error(e),
+            })?;
+        Poll::Ready(Ok(Self::BidiStream {
+            send: Self::SendStream::new(send),
+            recv: RecvStream::new(recv),
+        }))
+    }
+
+    fn poll_open_send(&mut self, cx: &mut task::Context<'_>) -> Poll<Result<Self::SendStream, StreamErrorIncoming>> {
+        let uni = self
+            .opening_uni
+            .get_or_insert_with(|| Box::pin(stream::unfold(self.conn.clone(), |conn| async { Some((conn.open_uni().await, conn)) })));
+
+        let send = ready!(uni.poll_next_unpin(cx))
+            .expect("BoxStream does not return None")
+            .map_err(|e| StreamErrorIncoming::ConnectionErrorIncoming {
+                connection_error: convert_connection_error(e),
+            })?;
+        Poll::Ready(Ok(Self::SendStream::new(send)))
+    }
+
+    fn close(&mut self, code: Code, reason: &[u8]) {
+        self.conn.close(VarInt::from_u64(code.value()).expect("error code VarInt"), reason);
+    }
+}
+
+/// Stream opener backed by a Quinn connection
+///
+/// Implements [`quic::OpenStreams`] using [`quinn::Connection`],
+/// [`quinn::OpenBi`], [`quinn::OpenUni`].
+pub struct OpenStreams {
+    conn: quinn::Connection,
+    opening_bi: Option<BoxStreamSync<'static, <OpenBi<'static> as Future>::Output>>,
+    opening_uni: Option<BoxStreamSync<'static, <OpenUni<'static> as Future>::Output>>,
+}
+
+impl<B> quic::OpenStreams<B> for OpenStreams
+where
+    B: Buf,
+{
+    type BidiStream = BidiStream<B>;
+    type SendStream = SendStream<B>;
+
+    fn poll_open_bidi(&mut self, cx: &mut task::Context<'_>) -> Poll<Result<Self::BidiStream, StreamErrorIncoming>> {
+        let bi = self
+            .opening_bi
+            .get_or_insert_with(|| Box::pin(stream::unfold(self.conn.clone(), |conn| async { Some((conn.open_bi().await, conn)) })));
+
+        let (send, recv) = ready!(bi.poll_next_unpin(cx))
+            .expect("BoxStream does not return None")
+            .map_err(|e| StreamErrorIncoming::ConnectionErrorIncoming {
+                connection_error: convert_connection_error(e),
+            })?;
+        Poll::Ready(Ok(Self::BidiStream {
+            send: Self::SendStream::new(send),
+            recv: RecvStream::new(recv),
+        }))
+    }
+
+    fn poll_open_send(&mut self, cx: &mut task::Context<'_>) -> Poll<Result<Self::SendStream, StreamErrorIncoming>> {
+        let uni = self
+            .opening_uni
+            .get_or_insert_with(|| Box::pin(stream::unfold(self.conn.clone(), |conn| async { Some((conn.open_uni().await, conn)) })));
+
+        let send = ready!(uni.poll_next_unpin(cx))
+            .expect("BoxStream does not return None")
+            .map_err(|e| StreamErrorIncoming::ConnectionErrorIncoming {
+                connection_error: convert_connection_error(e),
+            })?;
+        Poll::Ready(Ok(Self::SendStream::new(send)))
+    }
+
+    fn close(&mut self, code: Code, reason: &[u8]) {
+        self.conn.close(VarInt::from_u64(code.value()).expect("error code VarInt"), reason);
+    }
+}
+
+impl Clone for OpenStreams {
+    fn clone(&self) -> Self {
+        Self {
+            conn: self.conn.clone(),
+            opening_bi: None,
+            opening_uni: None,
+        }
+    }
+}
+
+/// Quinn-backed bidirectional stream
+///
+/// Implements [`quic::BidiStream`] which allows the stream to be split
+/// into two structs each implementing one direction.
+pub struct BidiStream<B>
+where
+    B: Buf,
+{
+    send: SendStream<B>,
+    recv: RecvStream,
+}
+
+impl<B> quic::BidiStream<B> for BidiStream<B>
+where
+    B: Buf,
+{
+    type RecvStream = RecvStream;
+    type SendStream = SendStream<B>;
+
+    fn split(self) -> (Self::SendStream, Self::RecvStream) {
+        (self.send, self.recv)
+    }
+}
+
+impl<B: Buf> quic::RecvStream for BidiStream<B> {
+    type Buf = Bytes;
+
+    fn poll_data(&mut self, cx: &mut task::Context<'_>) -> Poll<Result<Option<Self::Buf>, StreamErrorIncoming>> {
+        self.recv.poll_data(cx)
+    }
+
+    fn stop_sending(&mut self, error_code: u64) {
+        self.recv.stop_sending(error_code)
+    }
+
+    fn recv_id(&self) -> StreamId {
+        self.recv.recv_id()
+    }
+}
+
+impl<B> quic::SendStream<B> for BidiStream<B>
+where
+    B: Buf,
+{
+    fn poll_ready(&mut self, cx: &mut task::Context<'_>) -> Poll<Result<(), StreamErrorIncoming>> {
+        self.send.poll_ready(cx)
+    }
+
+    fn poll_finish(&mut self, cx: &mut task::Context<'_>) -> Poll<Result<(), StreamErrorIncoming>> {
+        self.send.poll_finish(cx)
+    }
+
+    fn reset(&mut self, reset_code: u64) {
+        self.send.reset(reset_code)
+    }
+
+    fn send_data<D: Into<WriteBuf<B>>>(&mut self, data: D) -> Result<(), StreamErrorIncoming> {
+        self.send.send_data(data)
+    }
+
+    fn send_id(&self) -> StreamId {
+        self.send.send_id()
+    }
+}
+impl<B> quic::SendStreamUnframed<B> for BidiStream<B>
+where
+    B: Buf,
+{
+    fn poll_send<D: Buf>(&mut self, cx: &mut task::Context<'_>, buf: &mut D) -> Poll<Result<usize, StreamErrorIncoming>> {
+        self.send.poll_send(cx, buf)
+    }
+}
+
+/// Quinn-backed receive stream
+///
+/// Implements a [`quic::RecvStream`] backed by a [`quinn::RecvStream`].
+pub struct RecvStream {
+    stream: Option<quinn::RecvStream>,
+    read_chunk_fut: ReadChunkFuture,
+}
+
+type ReadChunkFuture = ReusableBoxFuture<'static, (quinn::RecvStream, Result<Option<quinn::Chunk>, quinn::ReadError>)>;
+
+impl RecvStream {
+    fn new(stream: quinn::RecvStream) -> Self {
+        Self {
+            stream: Some(stream),
+            // Should only allocate once the first time it's used
+            read_chunk_fut: ReusableBoxFuture::new(async { unreachable!() }),
+        }
+    }
+}
+
+impl quic::RecvStream for RecvStream {
+    type Buf = Bytes;
+
+    fn poll_data(&mut self, cx: &mut task::Context<'_>) -> Poll<Result<Option<Self::Buf>, StreamErrorIncoming>> {
+        if let Some(mut stream) = self.stream.take() {
+            self.read_chunk_fut.set(async move {
+                let chunk = stream.read_chunk(usize::MAX, true).await;
+                (stream, chunk)
+            })
+        };
+
+        let (stream, chunk) = ready!(self.read_chunk_fut.poll(cx));
+        self.stream = Some(stream);
+        Poll::Ready(Ok(chunk.map_err(convert_read_error_to_stream_error)?.map(|c| c.bytes)))
+    }
+
+    fn stop_sending(&mut self, error_code: u64) {
+        self.stream
+            .as_mut()
+            .unwrap()
+            .stop(VarInt::from_u64(error_code).expect("invalid error_code"))
+            .ok();
+    }
+
+    fn recv_id(&self) -> StreamId {
+        let num: u64 = self.stream.as_ref().unwrap().id().into();
+
+        num.try_into().expect("invalid stream id")
+    }
+}
+
+fn convert_read_error_to_stream_error(error: ReadError) -> StreamErrorIncoming {
+    match error {
+        ReadError::Reset(var_int) => StreamErrorIncoming::StreamTerminated {
+            error_code: var_int.into_inner(),
+        },
+        ReadError::ConnectionLost(connection_error) => StreamErrorIncoming::ConnectionErrorIncoming {
+            connection_error: convert_connection_error(connection_error),
+        },
+        error @ ReadError::ClosedStream => StreamErrorIncoming::Unknown(Box::new(error)),
+        ReadError::IllegalOrderedRead => panic!("h3-quinn only performs ordered reads"),
+        error @ ReadError::ZeroRttRejected => StreamErrorIncoming::Unknown(Box::new(error)),
+    }
+}
+
+fn convert_write_error_to_stream_error(error: quinn::WriteError) -> StreamErrorIncoming {
+    match error {
+        quinn::WriteError::Stopped(var_int) => StreamErrorIncoming::StreamTerminated {
+            error_code: var_int.into_inner(),
+        },
+        quinn::WriteError::ConnectionLost(connection_error) => StreamErrorIncoming::ConnectionErrorIncoming {
+            connection_error: convert_connection_error(connection_error),
+        },
+        error @ quinn::WriteError::ClosedStream | error @ quinn::WriteError::ZeroRttRejected => {
+            StreamErrorIncoming::Unknown(Box::new(error))
+        }
+    }
+}
+
+/// Quinn-backed send stream
+///
+/// Implements a [`quic::SendStream`] backed by a [`quinn::SendStream`].
+pub struct SendStream<B: Buf> {
+    stream: quinn::SendStream,
+    writing: Option<WriteBuf<B>>,
+    stopped: Option<BoxFuture<'static, Result<Option<quinn::VarInt>, quinn::StoppedError>>>,
+}
+
+impl<B> SendStream<B>
+where
+    B: Buf,
+{
+    fn new(stream: quinn::SendStream) -> SendStream<B> {
+        Self {
+            stream,
+            writing: None,
+            stopped: None,
+        }
+    }
+}
+
+impl<B> quic::SendStream<B> for SendStream<B>
+where
+    B: Buf,
+{
+    fn poll_ready(&mut self, cx: &mut task::Context<'_>) -> Poll<Result<(), StreamErrorIncoming>> {
+        if let Some(ref mut data) = self.writing {
+            while data.has_remaining() {
+                let stream = Pin::new(&mut self.stream);
+                let written = ready!(stream.poll_write(cx, data.chunk())).map_err(convert_write_error_to_stream_error)?;
+                data.advance(written);
+            }
+        }
+        // all data is written
+        self.writing = None;
+        Poll::Ready(Ok(()))
+    }
+
+    fn poll_finish(&mut self, cx: &mut task::Context<'_>) -> Poll<Result<(), StreamErrorIncoming>> {
+        let finish = Poll::Ready(self.stream.finish().map_err(|e| StreamErrorIncoming::Unknown(Box::new(e))));
+
+        // Wait all data received by the peer
+        let stopped = self.stopped.get_or_insert_with(|| Box::pin(self.stream.stopped()));
+        ready!(stopped.poll_unpin(cx)).map_err(|e| StreamErrorIncoming::Unknown(Box::new(e)))?;
+        self.stopped.take(); // Remove finish future
+
+        finish
+    }
+
+    fn reset(&mut self, reset_code: u64) {
+        let _ = self.stream.reset(VarInt::from_u64(reset_code).unwrap_or(VarInt::MAX));
+    }
+
+    fn send_data<D: Into<WriteBuf<B>>>(&mut self, data: D) -> Result<(), StreamErrorIncoming> {
+        if self.writing.is_some() {
+            // This can only happen if the traits are misused by h3 itself
+            // If this happens log an error and close the connection with H3_INTERNAL_ERROR
+
+            return Err(StreamErrorIncoming::ConnectionErrorIncoming {
+                connection_error: ConnectionErrorIncoming::InternalError("internal error in the http stack".to_string()),
+            });
+        }
+        self.writing = Some(data.into());
+        Ok(())
+    }
+
+    fn send_id(&self) -> StreamId {
+        let num: u64 = self.stream.id().into();
+        num.try_into().expect("invalid stream id")
+    }
+}
+
+impl<B> quic::SendStreamUnframed<B> for SendStream<B>
+where
+    B: Buf,
+{
+    fn poll_send<D: Buf>(&mut self, cx: &mut task::Context<'_>, buf: &mut D) -> Poll<Result<usize, StreamErrorIncoming>> {
+        if self.writing.is_some() {
+            // This signifies a bug in implementation
+            panic!("poll_send called while send stream is not ready")
+        }
+
+        let s = Pin::new(&mut self.stream);
+
+        let res = ready!(s.poll_write(cx, buf.chunk()));
+        match res {
+            Ok(written) => {
+                buf.advance(written);
+                Poll::Ready(Ok(written))
+            }
+            Err(err) => Poll::Ready(Err(convert_write_error_to_stream_error(err))),
+        }
+    }
+}
